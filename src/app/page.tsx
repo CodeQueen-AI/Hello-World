7bef3a59b68a4266eb6d7afb3308b1e1696f2485
Sumbal Naz <Codeq209@gmail.com>
2024-12-27 15:34:01 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-white bg-gradient-to-l from-pink-600 via-pink-400 to-pink-900 flex items-center justify-center h-screen text-6xl font-extrabold'>
+      Hello World!
+    </div>
+  )
+}
+
+export default page
